dddc50a8ada26497f637cfdd2adc31a3082e6342
anazuleica <anazuleica2@gmail.com>
2022-12-11 11:59:00 -0300

    Autenticação User v0

--- index.js
+++ index.js
@@ -1,29 +1,109 @@
 const express = require("express");
 const session = require("express-session");
 const hbs = require("express-handlebars");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const { redirect } = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 3000;
-
-//BodyParser
-app.use(bodyParser.json());
 
 //Dotenv
 require("dotenv/config");
 
 //Login
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
+const passport = require("passport");
+var crypto = require("crypto");
 
 //Middleware
 app.engine("hbs", hbs.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Credentials Mongoose
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose
+	.connect(
+		`mongodb+srv://${dbUser}:${dbPassword}@cluster0.iqzkooa.mongodb.net/?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => {
+		console.log("Conectado ao banco");
+	})
+	.catch((err) => console.log(err.message));
+
+//Models
+const User = require("./public/models/user");
+
+//Login User
+app.post("/auth/login", async (req, res) => {
+	const { username, password } = req.body;
+
+	const user = await User.findOne({ username }).lean();
+
+	if (!user) {
+		return res.json({ status: "error", error: "Usuário não encontrado" });
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username,
+			},
+			process.env.JWT_SECRET
+		);
+		return res.json({ status: "ok", data: token });
+	}
+
+	res.json({ status: "error", error: "Usuário ou senha inválidos" });
+});
+
+//Register User
+app.post("/auth/register", async (req, res) => {
+	const { username, password: PlainTextPassword } = req.body;
+
+	if (!username || typeof username !== "string") {
+		return res.json({ status: "error", error: "Nome de usuário inválido" });
+	}
+
+	if (!PlainTextPassword || typeof PlainTextPassword !== "string") {
+		return res.json({ status: "error", error: "Senha inválida" });
+	}
+
+	if (PlainTextPassword.length < 5) {
+		return res.json({
+			status: "error",
+			error: "A senha deve ter no mínimo 5 caracteres",
+		});
+	}
+
+	const password = await bcrypt.hash(PlainTextPassword, 10);
+
+	try {
+		const response = await User.create({
+			username,
+			password,
+		});
+		console.log("Usuário criado com sucesso", response);
+	} catch (error) {
+		if (error.code === 11000) {
+			//Usuário duplicado
+			return res.json({ status: "error", error: "Nome de usuário já existe" });
+		}
+		throw error;
+	}
+
+	res.json({ status: "ok" });
+});
 
 //Routes
 app.get("/", (req, res) => {
@@ -78,88 +158,6 @@ app.get("/:id/season/:id/episode/:id", (req, res) => {
 	res.render("episode.hbs", {
 		style: "../../../../../css/episode.css",
 	});
-});
-
-//Credentials
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-
-mongoose
-	.connect(
-		`mongodb+srv://${dbUser}:${dbPassword}@cluster0.iqzkooa.mongodb.net/?retryWrites=true&w=majority`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}
-	)
-	.then(() => {
-		console.log("Conectado ao banco");
-	})
-	.catch((err) => console.log(err.message));
-
-//Models
-const User = require("./public/models/user");
-
-//Login User
-app.post("/auth/login", async (req, res) => {
-	const { username, password } = req.body;
-
-	const user = await User.findOne({ username }).lean();
-
-	if (!user) {
-		return res.json({ status: "error", error: "Usuário não encontrado" });
-	}
-
-	if (await bcrypt.compare(password, user.password)) {
-		const token = jwt.sign(
-			{
-				id: user._id,
-				username: user.username,
-			},
-			process.env.JWT_SECRET
-		);
-		return res.json({ status: "ok", data: token });
-	}
-
-	res.json({ status: "error", error: "Usuário ou senha inválidos" });
-});
-
-//Register User
-app.post("/register", async (req, res) => {
-	const { username, password: PlainTextPassword } = req.body;
-
-	if (!username || typeof username !== "string") {
-		return res.json({ status: "error", error: "Nome de usuário inválido" });
-	}
-
-	if (!PlainTextPassword || typeof PlainTextPassword !== "string") {
-		return res.json({ status: "error", error: "Senha inválida" });
-	}
-
-	if (PlainTextPassword.length < 5) {
-		return res.json({
-			status: "error",
-			error: "A senha deve ter no mínimo 5 caracteres",
-		});
-	}
-
-	const password = await bcrypt.hash(PlainTextPassword, 10);
-
-	try {
-		const response = await User.create({
-			username,
-			password,
-		});
-		console.log("Usuário criado com sucesso", response);
-	} catch (error) {
-		if (error.code === 11000) {
-			//Usuário duplicado
-			return res.json({ status: "error", error: "Nome de usuário já existe" });
-		}
-		throw error;
-	}
-
-	res.json({ status: "ok" });
 });
 
 //Listen
